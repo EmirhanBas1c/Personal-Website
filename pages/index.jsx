07c67306803a9f2daaebb5ef1dbc91efc945f8b2
EmirhanBas1c <123837275+EmirhanBas1c@users.noreply.github.com>
2023-04-27 20:14:54 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,50 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import swr from '../lib/swr';
-import Tippy from '@tippyjs/react';
+import ErrorPage from '../components/ErrorPage';
+const UnkownPage = () => {
+    let messages = [
+        'Have you lost your way, kiddo?',
+        'Damn it! This dead-end road.',
+        'The Aurors blocked this road, Harry!'
+    ];
 
-
-export default function Home() {
-  const { data: _repositories } = swr("/api/repos");
-  const repositories = _repositories ? _repositories : null;
-
-  return (
-    <>
-      <div className="bg-neutral-800/10 shadow-xl rounded-lg w-full h-auto mt-6">
-          <div className="relative">
-            <div className="flex flex-col lg:flex-row justify-between w-full p-6 px-8 items-center h-full">
-              <div className="flex flex-col lg:justify-start justify-center items-center lg:items-start mt-5 lg:mt-0 w-full">
-                <div className="flex items-center">
-                <p className="flex items-center text-white text-4xl font-semibold">
-                  Emirhan Özpınar
-                </p>
-                  <Tippy content={`Online`} animation="shift-away" arrow={false}>
-                    <span className={`ml-2 text-online px-2 py-1 font-normal rounded-md text-sm`}>
-                          <i className={`fa fa-circle text-online mr-2`} />Online
-                    </span>
-                  </Tippy>
-            
-                </div>
-                <p className="text-white/50 text-md mt-3">
-The Under Construction... / Yapım Aşamasında
-                </p>
-              </div>
-              <div className={`order-first lg:order-last flex-shrink-0 relative w-[160px] h-[160px] rounded-full pulse-avatar-online `}>
-                <img alt="umutbayraktar" src={`https://cdn.discordapp.com/attachments/1096542635335888958/1101151288508559370/239f8d12d0ba625cf8042079ea54d43806f2c070_full.jpg`} width="160" height="160" className={`bg-neutral-700 w-[160px] h-[160px] rounded-full`} />
-                <div className={`bg-[#040404] rounded-full px-[4px] py-[1px] flex items-center absolute bottom-0 right-4`}>
-                  <Tippy content="Online" animation="shift-away" arrow={false}>
-                    <i className={`fad fa-circle text-2xl text-online`} />
-                  </Tippy>
-                </div>
-              </div>
-              
-            </div>
-            <br></br>
-            <span style={{ zIndex: '-1' }} className="text-white/5 absolute bottom-3 left-7 text-xl sm:text-2xl md:text-4xl lg:text-3xl font-semibold">Community Manager</span>
-          </div>
-      </div>
-
-
-    </>
-  )
+    return <ErrorPage code={404} message={messages[Math.floor(Math.random()*messages.length)] || "Page not found."} />
 }
+
+export default UnkownPage;
+
