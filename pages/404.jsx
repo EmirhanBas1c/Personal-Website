219b5ed42d98ef650459378d8802307ab7f1d5b6
EmirhanBas1c <123837275+EmirhanBas1c@users.noreply.github.com>
2023-04-27 20:39:15 +0300

    Update 404.jsx

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,9 +1,9 @@
 import ErrorPage from '../components/ErrorPage';
 const UnkownPage = () => {
     let messages = [
-        'Have you lost your way, kiddo?',
-        'Damn it! This dead-end road.',
-        'The Aurors blocked this road, Harry!'
+        'Have you lost your way, kiddo? (Yapım Aşamasında.)',
+        'Damn it! This dead-end road. (Yapım Aşamasında.)',
+        'The Aurors blocked this road, Harry! (Yapım Aşamasında.)'
     ];
 
     return <ErrorPage code={404} message={messages[Math.floor(Math.random()*messages.length)] || "Page not found."} />
