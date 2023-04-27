a717773cfee6f0ac16536f1ee136d97ab26259a5
EmirhanBas1c <123837275+EmirhanBas1c@users.noreply.github.com>
2023-04-27 17:48:12 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 
 const items = [
-    { icon: 'fab fa-discord', link: 'https://discord.com/users/517023660972834847' },
     { icon: 'fab fa-instagram', link: 'https://instagram.com/hozpinar419' },
+    { icon: 'fab fa-discord', link: 'https://discord.com/users/517023660972834847' },
   ]
 
 const navItems = [
