ef2d0c4d52600071282f66e25b2050f15bbf5f45
EmirhanBas1c <123837275+EmirhanBas1c@users.noreply.github.com>
2023-10-22 12:12:05 +0300

    Update download.jsx

--- pages/download.jsx
+++ pages/download.jsx
@@ -10,14 +10,14 @@ export default function Home() {
   return (
     <>
       <div className="py-20">
-        <p className="text-3xl text-white font-semibold text-center">Contact Me</p>
-        <p className="text-xl text-white/50 font-normal text-center mb-5">Other addresses where you can contact me.</p>
+        <p className="text-3xl text-white font-semibold text-center">Mod Paketi</p>
+        <p className="text-xl text-white/50 font-normal text-center mb-5">İstediğiniz Linkten İndirebilirsiniz.</p>
         <div className=" flex justify-center flex-col items-center">
             <div className="max-w-lg w-full">
                 <a target="_blank" rel="norefferer" href=" https://instagram.com/hozpinar419/">
                     <div className="flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
-                        <i className="fab fa-instagram fa-2x" />
-                        <p className="font-semibold text-xl">@hozpinar419</p>
+                        <i className="fab fa-drive fa-2x" />
+                        <p className="font-semibold text-xl">Google Drive</p>
                     </div>
                 </a>
                 <a target="_blank" rel="norefferer" href="https://discord.com/users/517023660972834847">
