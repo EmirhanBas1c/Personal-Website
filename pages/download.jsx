ffa92bb6b56d893f7bb308e24484fa48b7aa81b4
EmirhanBas1c <123837275+EmirhanBas1c@users.noreply.github.com>
2023-10-22 12:28:46 +0300

    Update download.jsx

--- pages/download.jsx
+++ pages/download.jsx
@@ -22,7 +22,7 @@ export default function Home() {
                 </a>
                 <a target="_blank" rel="norefferer" href="https://discord.com/users/517023660972834847">
                     <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
-                        <i class="fa-regular fa-fire fa-2x"></i>
+                        <i className="fab fa-fire fa-2x" />
                         <p className="font-semibold text-xl">Link 2 » MediaFire</p>
                     </div>
                 </a>
