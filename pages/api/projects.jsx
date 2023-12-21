0c9dc2f1431fa8f348ef34e22b76792ea0bc6c6b
EmirhanBas1c <123837275+EmirhanBas1c@users.noreply.github.com>
2023-12-21 19:43:07 +0300

    Update projects.jsx

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -2,7 +2,7 @@ export default async (req, res) => {
   const data = [
     {
       id: 1,
-      image: "https://media.discordapp.net/attachments/737348411568685066/954502826791428136/codeshare_logo.png",
+      image: "",
       name: "Sunu PDF Dosyası",
       description: ".",
       link: "https://drive.google.com/file/d/1KKdvkS6SmOVjLY8dptwiW6mGuH6pZBgD/view?usp=sharing",
