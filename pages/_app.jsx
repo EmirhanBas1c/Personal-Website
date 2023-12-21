e53068385dfe41c564f49ae389e62a289f3f8e7d
EmirhanBas1c <123837275+EmirhanBas1c@users.noreply.github.com>
2023-12-21 19:55:03 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,52 +1,88 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import swr from '../lib/swr';
+import '../styles/globals.css'
+import '../styles/tooltip.css'
+import Head from 'next/head';
+import dynamic from 'next/dynamic';
+import 'tippy.js/animations/scale-subtle.css';
+import 'tippy.js/animations/scale-extreme.css';
+import 'tippy.js/animations/shift-away.css';
+import 'tippy.js/animations/shift-toward.css';
+import Router, { useRouter } from "next/router";
+import { Fragment, useEffect, useState } from "react";
+import { Transition } from "@headlessui/react";
 
+const Header = dynamic(() => import('../components/Header'))
+function MyApp({ Component, pageProps }) {
+  let [load,setLoad] = useState(false);
+    useEffect(() => {
+      setTimeout(() => {
+        setLoad(true);
+        document.documentElement.style = 'pointer-events: all;'
+      }, 1000)
+    }, [])
+    Router.events.on('routeChangeStart', () => {
+        setLoad(false);
+        document.documentElement.style = 'pointer-events: none;'
+    });
+    Router.events.on('routeChangeComplete', () => {
+      setTimeout(() => {
+        setLoad(true);
+        document.documentElement.style = 'pointer-events: all;'
+      }, 1000)
+    });
+    Router.events.on('routeChangeError', () => {
+      setTimeout(() => {
+        setLoad(true);
+        document.documentElement.style = 'pointer-events: all;'
+      }, 1000)
+    });
+  return (<>
+    <Head>
+        <title>Emirhan Özpınar</title>
+        <link rel="shortcut icon" href="https://cdn.discordapp.com/attachments/1096542635335888958/1101151288508559370/239f8d12d0ba625cf8042079ea54d43806f2c070_full.jpg" type="image/x-icon" ></link>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="https://pro.fontawesome.com/releases/v5.15.4/css/all.css" rel="stylesheet" />
 
-export default function Home() {
-  const { data: _projects } = swr('/api/projects');
-  const projects = _projects ? _projects : null;
-
-  return (
-    <>
-      <div className="py-20">
-        <p className="text-3xl text-white font-semibold text-center">My Projects</p>
-        <p className="text-xl text-white/50 font-normal text-center mb-5">The projects that I am actively working on are in this field.</p>
-
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 w-full gap-4 items-center mt-2">
-          {_projects ? (
-            projects ? (
-              projects.map((_, __) => (
-                <a href={_.link} target="_blank" rel="noreferrer" key={__} className="bg-neutral-800/10 p-4 hover:bg-neutral-800/20 shadow-lg hover:shadow-xl transition-all duration-200 rounded-lg w-full">
-                  <img alt="umut" src={_.image} width="1024" className="rounded-lg" height="512" />
-                  <p className="text-xl font-semibold mt-5">{_.name}</p>
-                  <p className="text-md font-normal text-white/50 h-24 overflow-small" style={{ "overflow": 'auto' }}>{_.description}</p>
-                </a>
-              ))
-            ) : (
-              Array.from({ length: 3 }).map((_, __) => (
-                <div key={__} className="bg-neutral-800/20 p-4 rounded-lg w-full">
-                  <div className="flex-shrink-0 w-[100%] h-[8.6rem]">
-                    <div className="w-full h-full bg-neutral-700/30 rounded-lg animate-pulse" />
-                  </div>
-                  <div className="bg-neutral-700/30 animate-pulse w-1/2 h-[24px] rounded-md mt-5" />
-                  <div className="mt-2 bg-neutral-700/30 animate-pulse w-full h-[96px] rounded-md" />
+    </Head>
+    <Transition
+          as={Fragment}
+          show={!load ? true : false}
+          enter="transform transition duration-[100ms]"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transform duration-[250ms] transition ease-in-out"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+      >
+        <div style={{ zIndex: 99999 }} className="fixed bg-black/75 w-full h-screen flex justify-center items-center pointer-events-none">
+            <div className="flex items-center gap-x-6 animate-pulse">
+                <div className="text-center">
+                    <p className="text-6xl mb-5 font-semibold">Hozpinar419</p>
+                    <p className="uppercase text-xl font-semibold text-white"><i className="fal fa-spinner-third fa-spin" /></p>
                 </div>
-              ))
-            )
-          ) : (
-            Array.from({ length: 3 }).map((_, __) => (
-              <div key={__} className="bg-neutral-800/20 p-4 rounded-lg w-full">
-                <div className="flex-shrink-0 w-[100%] h-[8.6rem]">
-                  <div className="w-full h-full bg-neutral-700/30 rounded-lg animate-pulse" />
-                </div>
-                <div className="bg-neutral-700/30 animate-pulse w-1/2 h-[24px] rounded-md mt-5" />
-                <div className="mt-2 bg-neutral-700/30 animate-pulse w-full h-[96px] rounded-md" />
-              </div>
-            ))
-          )}
+            </div>
+        </div>
+    </Transition>
+    <main className="border-b-[7px] border-t-[7px] h-full border-neutral-800/50 w-full">
+      <div className="min-h-screen max-w-screen-lg p-5 w-full md:w-10/12 lg:w-8/12 mx-auto transition-all duration-300">
+        <Header />
+        <Component {...pageProps} />
+      </div>
+      <div className="bg-neutral-800/5">
+        <div className="max-w-screen-lg p-5 w-full md:w-10/12 lg:w-8/12 mx-auto transition-all duration-300">
+          <div className="md:flex w-full items-center justify-between">
+            <div>
+              <p>❤️ Emirhan Özpınar</p></div>
+            <div className="mt-2 md:mt-0 flex items-center">
+              <a href="https://www.instagram.com/hozpinar419" target="_blank" rel="noreferrer" className="w-full md:w-auto bg-neutral-700/5 hover:bg-neutral-700/20 px-4 py-2 rounded-md transition-all duration-200">
+                <i className="fab fa-instagram mr-2" />Follow Me
+              </a>
+            </div>
+
+          </div>
         </div>
       </div>
-    </>
-  )
+    </main>
+  </>);
 }
+
+export default MyApp
